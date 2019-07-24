c3738922dbf93afe742a3043007b649eb1368ca9
mridulganga <mridul.kepler@gmail.com>
2019-07-24 13:27:09 +0530

    setting port to 80

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def main():
     # updater.start_polling()
 
     updater.start_webhook(listen="0.0.0.0",
-                      port=8443,
+                      port=80,
                       url_path=token)
     updater.bot.set_webhook("https://telegram-mg-bot.herokuapp.com/" + token)
 
