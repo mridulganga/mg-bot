5a2d8eab3355fe701e3150c6acd29ec2f70cc886
mridulganga <mridul.kepler@gmail.com>
2019-07-26 09:32:23 +0530

    try 2

--- app/help.py
+++ app/help.py
@@ -5,14 +5,14 @@ from app.db import get_help
 def help_handler(bot, update, msg_list):
     if len(msg_list) > 2:
         help_obj = get_help(msg_list[2])
-        help_str = "*" + help_obj["title"] + "*\n" + help_obj["description"].decode()
+        help_str = "*" + help_obj["title"] + "*\n" + help_obj["description"].encode("utf-8").decode("unicode_escape")
         
         bot.send_message(chat_id=update.message.chat_id, 
                 text=help_str, 
                 parse_mode=telegram.ParseMode.MARKDOWN)
     else:
         help_obj = get_help("*")
-        help_str = "*" + help_obj["title"] + "*\n" + help_obj["description"].decode()
+        help_str = "*" + help_obj["title"] + "*\n" + help_obj["description"].encode("utf-8").decode("unicode_escape")
         bot.send_message(chat_id=update.message.chat_id, 
                 text=help_str, 
                 parse_mode=telegram.ParseMode.MARKDOWN)
