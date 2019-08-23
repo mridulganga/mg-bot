92a8ccea30844dbc25e5499c70ab21825a15155e
mridulganga <mridul.kepler@gmail.com>
2019-08-23 19:46:05 +0530

    adding logs

--- app/admin.py
+++ app/admin.py
@@ -1,18 +1,23 @@
 from app.db import *
+from app.logger import logger
 
 def admin_handler(bot, update, msg_list):
     chat_id = update.message.chat_id
+    logger.debug("Reached handler")
     if bot.get_member(chat_id, update.message.from_user.id).status in ['creator', 'administrator']:
         is_admin=True
+        logger.debug("User is admin")
     else:
         update.message.reply_text("You are not admin.")
+        logger.debug("User not admin")
         return
     
     if len(msg_list) > 2:
-
+        
         # clear loan
         # pls admin loan clear @mridul
         if msg_list[2] == "loan":
+            logger.debug("clearing loan")
             user = msg_list[4].replace("@","")
             clear_loan(chat_id, user)
             update.message.reply_text("Loan has been cleared for " + user)
