81663898621178cf8d36cbe90d691c8d18573130
mridulganga <mridul.kepler@gmail.com>
2019-08-23 20:11:37 +0530

    test

--- app/administrator.py
+++ app/administrator.py
@@ -2,27 +2,28 @@ from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 from app.db import *
 
 def admin_handler(bot, update, msg_list):
-    chat_id = update.message.chat_id
+    update.message.reply_text("Test")
+    # chat_id = update.message.chat_id
 
-    if bot.get_member(chat_id, update.message.from_user.id).status in ['creator', 'administrator']:
-        is_admin=True
+    # if bot.get_member(chat_id, update.message.from_user.id).status in ['creator', 'administrator']:
+    #     is_admin=True
 
-    else:
-        update.message.reply_text("You are not admin.")
+    # else:
+    #     update.message.reply_text("You are not admin.")
 
-        return
+    #     return
     
-    if len(msg_list) > 2:
+    # if len(msg_list) > 2:
         
-        # clear loan
-        # pls admin loan clear @mridul
-        if msg_list[2] == "loan":
+    #     # clear loan
+    #     # pls admin loan clear @mridul
+    #     if msg_list[2] == "loan":
 
-            user = msg_list[4].replace("@","")
-            clear_loan(chat_id, user)
-            update.message.reply_text("Loan has been cleared for " + user)
+    #         user = msg_list[4].replace("@","")
+    #         clear_loan(chat_id, user)
+    #         update.message.reply_text("Loan has been cleared for " + user)
 
-        # ban user
-        # pls admin ban @mridul
-        if msg_list[2] == "ban":
-            user = msg_list[3].replace("@","")
+    #     # ban user
+    #     # pls admin ban @mridul
+    #     if msg_list[2] == "ban":
+    #         user = msg_list[3].replace("@","")
